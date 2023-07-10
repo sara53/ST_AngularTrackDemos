aa7a9b204f2913b909eef60d2bbc7454a7adb1f9
sara <sara.atef001@gmail.com>
2023-07-10 12:30:51 +0300

    day03

---
+++ Day03(JS)/demo.js
@@ -0,0 +1,165 @@
+// var firstName = "mona"; // string
+
+// var firstName = 12049; //
+
+// console.log(firstName);
+// firstName = "ali";
+// firstName = "hoda";
+
+// firstName = 11;
+// firstName = true;
+
+// var flag = true;
+// var num = 10.3445;
+// var test;
+
+// var student = null;
+
+// student = "ali"; //
+// console.log(typeof test);
+// console.log(typeof student); //
+
+/** Primitive datatype VS Refernce DataType
+ * ========================================
+ * Primitve DataType
+ * ---------------------
+ *  === Number
+ *  === String
+ *  === Boolean
+ *  === undefined
+ *  === null
+ *
+ */
+/**======================================== */
+/**
+ * 1- (+,-,)
+ *-----------------------------------------
+  ====> (+) ==> number + number --> add
+            ==> string + string --> concat
+ *
+    Coersion ==> convert automatic from datatype to anther datatype
+ */
+/** NaN ==> Not a Number */
+
+// var num = 10; // number "10"
+// var str = "mona"; // string
+
+// console.log(num - str);
+// var num1 = "Welcome"; // number
+// var num2 = "From ITI"; // number
+
+// var result = num1 + " " + num2;
+
+// console.log(result);
+/**==========
+ * Number() ==>
+ * String() ==>
+ * Boolean() ==>  true , false
+ * --------------Falsy Values -----------------
+ *  0 , false , "", null, undefined,NAN
+ * --------------------------------------------
+ */
+// var num1 = Number("mona");
+// console.log(typeof num1);
+/**
+ * &&
+ * == true && true --> true
+ * == true && false --> false
+ * == false && true --> false
+ * == false && false --> false
+ * ||
+ *------------------------------
+ == true || true --> true
+ * == true || false --> true
+ * == false || true --> true
+ * == false || false --> false
+ *---------------------------
+ !
+    true --> false 
+    false --> true
+-----------------------------------
+ */
+/**============== Comparasion Operator
+ * >
+ * <
+ * == ---> (only Values)
+ * ,
+ * === --->(DataTypes && Values)
+ * ==================== */
+
+// var num1 = "true"; //NaN
+// var num2 = true; // 1
+// console.log(num1 == num2);
+/**=============
+ * If,
+ * if else
+ * Switch --> (Search)
+ * ================== */
+
+// var color = "red";
+// var num = 10;
+// if (color == "red") {
+// 	console.log("Your Fav Color is red");
+// } else if (color == "green") {
+// 	console.log("Your Fav Color is green");
+// } else {
+// 	console.log("You don't have any Fav Colors");
+// }
+
+/** Switch  */
+/**
+ * while
+ * do while
+ * for
+ */
+
+// var i = 50;
+// while (i < 10) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// var i = 50;
+// do {
+// 	console.log(i);
+// 	i++;
+// } while (i < 10);
+// for (var i = 0; i <= 10; i += 2) {
+// 	console.log(i);
+// }
+// console.log("After Loop");
+// var i = 9;
+// document.writeln("<h1 class='test'>Welcome From iti</h1>");
+// document.writeln("<h" + i + ">Hello JS</h" + i + ">");
+// var num = prompt("enter Your Number"); //string
+// console.log(Number(num) + 10);
+// document.writeln("Your Name is :" + fname);
+/**
+ * Take two numbers of users
+ * --------------------------
+ * 1- [Sum,-,*,/] the two number and show result
+ * 10,20
+ * Result:
+ *      10+20 = 30
+ *      10-20 = -10
+ *      10*20 = 200
+ *      ...
+ * =================================
+ *  2- take number from user
+ *  1- check num is  positive or negative
+ *  2- check num is odd , even
+ *  3- FizzBuzzProblem
+ *     ==  % 3--> Fizz
+ *     == %5 --> Buzz
+ *     ==%3,5 --> fizzBuzz
+ *    --> None
+=================================
+ 4- take num from user  4
+<h1>Mona</h1>;
+<h2>Mona</h2>;
+<h3>Mona</h3>;
+<h4>Mona</h4>;
+
+
+
+ */
